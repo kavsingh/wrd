66765242bc3fe6f20f851a48636267aacedf133a
Kav Singh <flakisms@gmail.com>
2025-02-13 17:14:09 +0100

    feat: split cli into commands

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,69 @@
 mod data;
 mod matcher;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use matcher::match_from_pattern;
 
 use crate::matcher::{MatchOperation, MatchPattern};
 
-// wrd -p *_b_!ar_!r_* -e stlen -i ard
-
 /// find those words
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct Args {
-	/// search pattern
-	#[arg(short, long)]
-	pattern: String,
+struct Cli {
+	#[command(subcommand)]
+	command: Option<Commands>,
+}
 
-	/// letters to always exclude
-	#[arg(short, long, default_value_t = ("").to_string())]
-	exclude: String,
+#[derive(Subcommand, Debug)]
+enum Commands {
+	/// find matches from patterns
+	Match {
+		/// match pattern
+		/// _: seperator for char positions
+		/// *: match anything in this position
+		/// any number of lowercase chars: match any of the chars in the string in this position
+		/// ! + any number of lowercase chars: exclude any of the chars in the string in this position
+		///
+		/// e.g.
+		///   *_b_!ar_!r_*
+		///
+		///   1st position - match anything
+		///   2nd position - match 'b'
+		///   3rd position - do not match 'a' or 'r'
+		///   4th position - do not match 'r'
+		///   5th position - match anything
+		#[arg(short, long)]
+		pattern: String,
 
-	/// letters to always include
-	#[arg(short, long, default_value_t = ("").to_string())]
-	include: String,
+		/// letters to always exclude
+		#[arg(short, long, default_value_t = ("").to_string())]
+		exclude: String,
+
+		/// letters to always include
+		#[arg(short, long, default_value_t = ("").to_string())]
+		include: String,
+	},
 }
 
 fn main() {
-	let args = Args::parse();
-	let pattern = parse_pattern(&args.pattern);
-	let include: Vec<&str> = args.include.split("").filter(|c| !c.is_empty()).collect();
-	let exclude: Vec<&str> = args.exclude.split("").filter(|c| !c.is_empty()).collect();
+	let cli = Cli::parse();
+
+	match &cli.command {
+		Some(Commands::Match {
+			pattern,
+			exclude,
+			include,
+		}) => {
+			match_command(pattern, include, exclude);
+		}
+		None => panic!("expected a command"),
+	}
+}
+
+fn match_command(pattern: &str, include: &str, exclude: &str) {
+	let pattern = parse_pattern(pattern);
+	let include: Vec<&str> = include.split("").filter(|c| !c.is_empty()).collect();
+	let exclude: Vec<&str> = exclude.split("").filter(|c| !c.is_empty()).collect();
 	let result = match_from_pattern(&pattern, &include, &exclude);
 
 	println!("{}", result.join("\n"))
