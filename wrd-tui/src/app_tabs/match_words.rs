00e3051e8ad6fd34fbdb8d09dd2df74da322f224
Kav Singh <flakisms@gmail.com>
2025-05-30 01:46:59 +0200

    ugh

--- wrd-tui/src/app_tabs/match_words.rs
+++ wrd-tui/src/app_tabs/match_words.rs
@@ -1,13 +1,12 @@
-use std::sync::Arc;
-
 use color_eyre::eyre::Result;
 use ratatui::buffer::Buffer;
-use ratatui::crossterm::event::{self, Event, KeyCode, KeyEvent};
+use ratatui::crossterm::event::{Event, KeyCode, KeyEvent};
 use ratatui::layout::Constraint::{Length, Min};
 use ratatui::layout::{Layout, Rect};
 use ratatui::symbols::border;
 use ratatui::text::Text;
-use ratatui::widgets::{Block, Padding, Paragraph, Row, Table, Widget};
+use ratatui::widgets::{Block, Padding, Paragraph, Widget};
+use tui_input::Input;
 
 use super::{AppTab, AppTabIo};
 use crate::widgets::WordGrid;
@@ -26,10 +25,10 @@ enum TargetInput {
 pub struct MatchWords<'a> {
 	is_active: bool,
 	target_input: TargetInput,
-	pattern: String,
-	within: String,
-	include: String,
-	exclude: String,
+	pattern_input: Input,
+	within_input: Input,
+	include_input: Input,
+	exclude_input: Input,
 	results: Vec<&'static str>,
 	word_grid: WordGrid<'a>,
 }
@@ -38,21 +37,25 @@ const LABEL: &str = "Match";
 
 impl Default for MatchWords<'_> {
 	fn default() -> Self {
+		let mut word_grid = WordGrid::default();
 		let pattern = "* * * **";
 		let within = "trubador";
 		let include = "d";
 		let exclude = "";
+		let results =
+			wrd_lib::match_words(pattern, include, exclude, within, None).unwrap_or_default();
 
-		MatchWords {
+		word_grid.update(&results);
+
+		Self {
 			target_input: TargetInput::default(),
 			is_active: false,
-			pattern: pattern.into(),
-			within: within.into(),
-			include: include.into(),
-			exclude: exclude.into(),
-			results: wrd_lib::match_words(pattern, include, exclude, within, None)
-				.unwrap_or_default(),
-			word_grid: WordGrid::default(),
+			pattern_input: Input::new(pattern.into()),
+			within_input: Input::new(within.into()),
+			include_input: Input::new(include.into()),
+			exclude_input: Input::new(exclude.into()),
+			results,
+			word_grid,
 		}
 	}
 }
@@ -65,41 +68,59 @@ impl MatchWords<'_> {
 			KeyCode::Char('i') => self.target_input = TargetInput::Include,
 			KeyCode::Char('e') => self.target_input = TargetInput::Exclude,
 			KeyCode::Esc => self.target_input = TargetInput::None,
+			KeyCode::Enter => self.refresh_results(),
 			_ => (),
 		}
 
 		Ok(())
 	}
 
+	fn refresh_results(&mut self) {
+		let results = wrd_lib::match_words(
+			self.pattern_input.value(),
+			self.include_input.value(),
+			self.exclude_input.value(),
+			self.within_input.value(),
+			None,
+		)
+		.unwrap_or_default();
+
+		self.results = results;
+		self.word_grid.update(&self.results);
+	}
+
 	fn render_inputs(&self, area: Rect, buf: &mut Buffer) {
 		let block = Block::bordered()
 			.border_set(border::PLAIN)
-			.title(" inputs ");
-		let input_text = match self.target_input {
-			TargetInput::None => "None",
-			TargetInput::Pattern => "Pattern",
-			TargetInput::Within => "Within",
-			TargetInput::Include => "Include",
-			TargetInput::Exclude => "Exclude",
-		};
+			.title(" Inputs ");
+
+		let block_area = block.inner(area);
+
+		block.render(area, buf);
 
 		// block.render(area, buf);
-		Paragraph::new(Text::from(input_text))
-			.centered()
-			.block(block)
-			.render(area, buf);
+		let width = area.width.max(3) - 3;
+		let scroll = self.pattern_input.visual_scroll(width as usize);
+
+		Paragraph::new(self.pattern_input.value())
+			.scroll((0, scroll as u16))
+			.block(Block::bordered().title(" Pattern "))
+			.render(block_area, buf);
+
+		// if self.input_mode == InputMode::Editing {
+		// 	let x = self.pattern_input.visual_cursor().max(scroll) - scroll + 1;
+		// 	frame.set_cursor_position((area.x + x as u16, area.y + 1))
+		// }
 	}
 
 	fn render_results(&mut self, area: Rect, buf: &mut Buffer) {
 		let block = Block::bordered()
 			.border_set(border::PLAIN)
-			.title(" results ")
+			.title(" Results ")
 			.padding(Padding::horizontal(1));
 		let grid_area = block.inner(area);
 
 		block.render(area, buf);
-
-		self.word_grid.update(&self.results);
 		self.word_grid.clone().render(grid_area, buf);
 	}
 }
