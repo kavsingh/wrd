6c0008087a4e75a177cb1cd98ac795694de301bb
Kav Singh <flakisms@gmail.com>
2025-05-31 13:41:48 +0200

    attempt cursor, fail

--- wrd-tui/src/app_tabs/mod.rs
+++ wrd-tui/src/app_tabs/mod.rs
@@ -14,6 +14,7 @@ pub trait AppTabIo {
 	fn label(&self) -> &'static str;
 	fn set_active(&mut self, is_active: bool);
 	fn handle_event(&mut self, event: &Event) -> Result<()>;
+	fn get_cursor_position(&self) -> Option<(u16, u16)>;
 }
 
 impl std::fmt::Debug for dyn AppTabIo {
